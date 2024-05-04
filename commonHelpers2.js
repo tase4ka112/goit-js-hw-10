fcc66ff72ee9ee23cb1516c3e490537f7114095a
tase4ka112 <99361625+tase4ka112@users.noreply.github.com>
2024-05-04 20:38:41 +0000

    Deploying to gh-pages from @ tase4ka112/goit-js-hw-10@77258368b885faab6529e451071b155f500e990a 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as o}from"./assets/vendor-77e16229.js";const s=document.querySelector(".form");s.addEventListener("submit",function(r){r.preventDefault();const t=Number(s.delay.value),i=s.state.value;new Promise((e,m)=>{setTimeout(()=>{i==="fulfilled"?e(t):i==="rejected"&&m(t)},t)}).then(e=>{o.success({message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{o.error({message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=commonHelpers2.js.map
