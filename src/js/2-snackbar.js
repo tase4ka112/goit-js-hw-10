77258368b885faab6529e451071b155f500e990a
Tase4ka112 <tase4kakarpenko@gmail.com>
2024-05-04 23:38:17 +0300

    hw snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,35 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = Number(form.delay.value);
+  const stateInput = form.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (stateInput === 'fulfilled') {
+        resolve(delay);
+      } else if (stateInput === 'rejected') {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
